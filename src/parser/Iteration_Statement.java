d8b1c0200f36ecfe41a4a7ddf8e0492d090bdc74
Kidron Filbrun <kfilbrun@gmail.com>
2016-05-01 20:56:56 -0400

    Updated iteration statement

--- src/parser/Iteration_Statement.java
+++ src/parser/Iteration_Statement.java
@@ -3,6 +3,8 @@ package parser;
 import java.io.PrintWriter;
 import lowlevel.BasicBlock;
 import lowlevel.Function;
+import lowlevel.Operand;
+import lowlevel.Operation;
 
 public class Iteration_Statement extends Statement {
     //variable declarations
@@ -29,24 +31,46 @@ public class Iteration_Statement extends Statement {
 
     @Override
     void genLLCode(Function function) throws CodeGenerationException{
-        BasicBlock initialExprBlock = new BasicBlock(function);
-        function.appendToCurrentBlock(initialExprBlock);
-        function.setCurrBlock(initialExprBlock);
-        expression.genLLCode(function);
-        
+        //Create basic blocks
         BasicBlock thenBlock = new BasicBlock(function);
+        BasicBlock postBlock = new BasicBlock(function);
+        
+        //Gen code for the expression
+        expression.genLLCode(function);
+        //Set current block
+        BasicBlock curr = function.getCurrBlock();
+        
+        //Generate branch to post
+        genBranch(postBlock, curr);
+                
+        //Gen and append then block
         function.appendToCurrentBlock(thenBlock);
         function.setCurrBlock(thenBlock);
+        curr = function.getCurrBlock();
+        //Gen then code
         statement.genLLCode(function);
         
-        BasicBlock finalExprBlock = new BasicBlock(function);
-        function.appendToCurrentBlock(finalExprBlock);
-        function.setCurrBlock(finalExprBlock);
-        expression.genLLCode(function);
-        finalExprBlock.setNextBlock(thenBlock);
-        
-        BasicBlock postBlock = new BasicBlock(function);
+        //Generate looping branch to top of then
+        genBranch(thenBlock, curr);
+
+        //Append post block and set it as current to end
         function.appendToCurrentBlock(postBlock);
         function.setCurrBlock(postBlock);
     }
+    
+    void genBranch(BasicBlock targetBlock, BasicBlock curr){
+        int branchReg = expression.getRegNum();
+        Operand branchRegOp = new Operand(Operand.OperandType.REGISTER, branchReg);
+        Operand compRegOp = new Operand(Operand.OperandType.REGISTER, expression.getRegNum());
+        Operand tgtOp = new Operand(Operand.OperandType.BLOCK, targetBlock.getBlockNum());
+        
+        // add BEQ
+        Operation beqOper = new Operation(Operation.OperationType.BEQ, curr);
+        beqOper.setSrcOperand(0, branchRegOp);
+        beqOper.setSrcOperand(1, compRegOp);
+        beqOper.setDestOperand(0, tgtOp);
+        curr.appendOper(beqOper);
+    }
+    
+    
 }
